438edb94037bfd366878a4c87ed3b8e99e11053e
imoghul <moghul02@gmail.com>
2022-09-23 16:10:28 -0400

    added rudimentary instructions to README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # EcoPRT-IBM-Cloud-Fleet
 EcoPRT fleet management system with IBM Cloud
+
+
+
+## Usage
+``` bash
+cd catkin_ws # enter the catkin worksapce directory
+catkin_make # make the project
+source devel/setup.bash # change extension to match your terminal
+rosrun <node>/<file>
--- catkin_ws/build/testing/catkin_generated/installspace/talker.py
+++ catkin_ws/build/testing/catkin_generated/installspace/talker.py
@@ -4,8 +4,8 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    pub = rospy.Publisher('chatter', String, queue_size=10) // chatter is the topic
+    rospy.init_node('talker', anonymous=True) // talker is the node
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
