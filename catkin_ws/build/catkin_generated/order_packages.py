66f9c94ede375d5ad1e57d53a705ac449b0c4bae
imoghul <moghul02@gmail.com>
2023-01-18 11:23:04 -0500

    removed old pkgs

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/pi/EcoPRT-IBM-Cloud-Fleet/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/pi/EcoPRT-IBM-Cloud-Fleet/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/pi/EcoPRT-IBM-Cloud-Fleet/catkin_ws/devel;/opt/ros/noetic' != '' else []
