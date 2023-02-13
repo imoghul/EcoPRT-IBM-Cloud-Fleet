f5849240ccd466a983e428d8162ad7082a5b78b0
imoghul <moghul02@gmail.com>
2023-02-13 18:37:14 -0500

    file queueing finally working

--- catkin_ws/src/cloud/scripts/cloud/cloudQueue.py
+++ catkin_ws/src/cloud/scripts/cloud/cloudQueue.py
@@ -2,53 +2,78 @@ import requests
 import time
 import rospy
 import json
-from cloud.confidential import *
+import atexit
+import threading
+import cloud.confidential
+from datetime import datetime
+import roadquality.msg
+
+queueFile = (
+    "/home/pi/EcoPRT-IBM-Cloud-Fleet/catkin_ws/src/cloud/scripts/cloud/queue.json"
+)
+
+
+def rqScoreToJson(data: roadquality.msg.RoadQualityScore):
+    return {
+        "latitude": data.pos.gps.lat,
+        "longitude": data.pos.gps.long,
+        "road_quality_score": data.score,
+        "time": str(datetime.now()),
+    }
+
+
 class CloudQueue:
-    def __init__(self,name,tDiff=.5,_url = url):
+    def __init__(self, name, tDiff=0.5, url=cloud.confidential.url):
         self.name = name
         self.timeDiff = tDiff
-        self.url = _url
-    def addToQueue(self,data):
-        rfile = open("/home/pi/EcoPRT-IBM-Cloud-Fleet/catkin_ws/src/cloud/scripts/cloud/queue.json", 'r')
+        self.url = url
+        self.queue = self.getFileQueue()[self.name]
+        self.thread = threading.Thread(target=self.main)
+        self.__del__ = self.updateFile
+        atexit.register(self.updateFile)
+
+    def getFileQueue(self):
         try:
-            f = json.load(rfile)
-        except Exception as e:
-            f = {self.name:[]}
-        rfile.close()
-
-        ofile = open("/home/pi/EcoPRT-IBM-Cloud-Fleet/catkin_ws/src/cloud/scripts/cloud/queue.json",'w')
-        f[self.name].append(self.rqScoreToJson(data))
-        json.dump(f,ofile,indent=4)
-        ofile.close()
-    def main(self):
-        lastTime = time.time()
-        counter=0
-        while not rospy.is_shutdown():
-            counter+=1
-            if (time.time() >= lastTime):
+            open(queueFile,"r").close()
+        except: open(queueFile,"w").close()
+        with open(queueFile, "r") as rfile:
+            try:
+                return json.load(rfile)
+            except Exception as e:
                 try:
-                    readfile = open("/home/pi/EcoPRT-IBM-Cloud-Fleet/catkin_ws/src/cloud/scripts/cloud/queue.json",'r')
-                    f = json.load(readfile)
+                    rfile.read()
+                    return {self.name: []}
+                except Exception as e:
+                    raise Exception(
+                        "corrupt queue file"
+                    )
 
-                    readfile.close()
-                except Exception as e:pass
-                if (len(f[self.name]) > 0):
-                    self.putDataToCloud(f[self.name].pop(0))
-                    lastTime = time.time() + self.timeDiff
-                
-                outfile = open("/home/pi/EcoPRT-IBM-Cloud-Fleet/catkin_ws/src/cloud/scripts/cloud/queue.json",'w')
-                json.dump(f,outfile,indent=4)
-                outfile.close()
-    def rqScoreToJson(self,data):
-        return {
-                    "Latitude" : data.pos.gps.lat,
-                    "Longitude" : data.pos.gps.long,
-                    "road_quality_score": data.score
-        }
+    def updateFile(self):
+        f = self.getFileQueue()
+        with open(queueFile, "w") as ofile:
+            f[self.name] = self.queue.copy()
+            json.dump(f, ofile, indent=4)
 
-    def putDataToCloud(self,data):
-        print("Sent")
-        return
-        data["__passcode__"] = passcode 
-        return requests.post(self.url,json=data) 
+    def addToQueue(self, data: roadquality.msg.RoadQualityScore):
+        self.queue.append(rqScoreToJson(data))
+        self.updateFile()
+        print("Added")
 
+    def popFromQueue(self):
+        ret = self.queue.pop(0)
+        self.updateFile()
+        return ret
+
+    def main(self):
+        targetTime = time.time()
+
+        while not rospy.is_shutdown():
+            if time.time() >= targetTime:
+                if len(self.queue) > 0:
+                    self.putDataToCloud(self.popFromQueue())
+                    targetTime = time.time() + self.timeDiff
+
+    def putDataToCloud(self, data: dict):
+        rospy.loginfo("Sent to cloud")
+        data["__passcode__"] = cloud.confidential.passcode
+        return requests.post(self.url, json=data)
