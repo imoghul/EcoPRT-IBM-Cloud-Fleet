79e598d59745bbe8bedae9a287b538ec096b23d8
imoghul <moghul02@gmail.com>
2023-02-13 20:56:43 -0500

    removed builds

--- catkin_ws/devel_isolated/cloud/share/cloud/cmake/cloud-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(cloud_MSG_INCLUDE_DIRS "/home/pi/EcoPRT-IBM-Cloud-Fleet/catkin_ws/src/cloud/msg")
-set(cloud_MSG_DEPENDENCIES std_msgs;sensors;positioning;roadquality)
